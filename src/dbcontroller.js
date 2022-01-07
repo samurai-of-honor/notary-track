bb68091f4cfd78cb277401acba7fe2952ba509f6
Danil Bezus <37234379+danilbezus@users.noreply.github.com>
2022-01-07 15:32:45 +0200

    added dbcontroller, diagram and calendar

---
+++ src/dbcontroller.js
@@ -0,0 +1,27 @@
+export const ADODB = require('node-adodb');
+let filesource = "./src/Access.accdb";
+export const connection = ADODB.open(`Provider=Microsoft.ACE.OLEDB.12.0${filesource};Data Source=;Persist Security Info=False;`);
+
+ 
+export let DBController = {
+    getData: async function(query) {
+        try {
+          let users = await connection.query(query);
+
+          return await users;
+       
+          //console.log(JSON.stringify(users, null, 2));
+        } catch (error) {
+          console.log(error);
+        }
+    },
+    writeData: async function(query) {
+        try {
+          let users = await connection.execute(query);
+       
+          console.log(JSON.stringify(users, null, 2));
+        } catch (error) {
+          console.log(JSON.stringify(error, null, 2));
+        }
+    }
+}
